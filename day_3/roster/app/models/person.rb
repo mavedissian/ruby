a2ed27695ed33fb665d84693ff722bf4261299cb
Mickael Avedissian <mavedissian@ise.com>
2013-01-16 15:42:38 -0500

    adding validations

--- day_3/roster/app/models/person.rb
+++ day_3/roster/app/models/person.rb
@@ -1,3 +1,12 @@
 class Person < ActiveRecord::Base
   attr_accessible :first_name, :last_name
+
+ validates :first_name, :presence => true
+
+ before_create :set_defaults
+
+ def set_defaults
+  last_name.blank? ? self.last_name = "N/A" : last_name
+ end
+
 end
