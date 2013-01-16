4985440d5b6b2ce1c6fe1aa41baf27dd5baacf0c
Mickael Avedissian <mavedissian@ise.com>
2013-01-16 11:44:18 -0500

    Ruby on rails day 1
    Adding a directory for the first class of Ruby on rails (class day 3).

---
+++ day_3/roster/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roster::Application.config.secret_token = 'ad865d372a445d9e081aba57ac57424f60309def52cf7b6c2b292d55ab22c04f6887f2c7a76b4c9964fbbf4515ad00124add4360907562e59a1239293e82f7e2'
